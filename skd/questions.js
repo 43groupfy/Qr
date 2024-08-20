4def4fb195aeea328a0cb674bcd2b9ca9c8b3069
43groupfy <141533973+43groupfy@users.noreply.github.com>
2024-08-21 05:01:18 +0700

    Create questions.js

---
+++ skd/questions.js
@@ -0,0 +1,14 @@
+const questions = [
+    { category: 'A1', question: "Soal Matematika 1", options: ["A", "B", "C", "D", "E"], correct: 2 },
+    { category: 'A1', question: "Soal Matematika 2", options: ["A", "B", "C", "D", "E"], correct: 3 },
+    { category: 'B1', question: "Soal Fisika 1", options: ["A", "B", "C", "D", "E"], correct: 0 },
+    { category: 'B1', question: "Soal Fisika 2", options: ["A", "B", "C", "D", "E"], correct: 1 },
+    { category: 'C1', question: "Soal Biologi 1", options: ["A", "B", "C", "D", "E"], values: [1, 2, 3, 4, 5] },
+    { category: 'C1', question: "Soal Biologi 2", options: ["A", "B", "C", "D", "E"], values: [5, 4, 3, 2, 1] }, 
+    { category: 'A1', question: "Soal Matematika 1", options: ["A", "B", "C", "D", "E"], correct: 2 },
+    { category: 'A1', question: "Soal Matematika 2", options: ["A", "B", "C", "D", "E"], correct: 3 },
+    { category: 'B1', question: "Soal Fisika 1", options: ["A", "B", "C", "D", "E"], correct: 0 },
+    { category: 'B1', question: "Soal Fisika 2", options: ["A", "B", "C", "D", "E"], correct: 1 },
+    { category: 'C1', question: "Soal Biologi 1", options: ["A", "B", "C", "D", "E"], values: [1, 2, 3, 4, 5] },
+    { category: 'C1', question: "Soal Biologi 2", options: ["A", "B", "C", "D", "E"], values: [5, 4, 3, 2, 1] }
+];

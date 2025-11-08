e24be317223dcdef9f142fc5cc509c9387250920
43groupfy <141533973+43groupfy@users.noreply.github.com>
2025-11-08 18:06:38 +0700

    Create service-worker.js

---
+++ almeto/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'almeto-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
